23e2d56909d9f968b325ac2a81603a8a60acdd70
Jacob1943 <jacobyip1943@gmail.com>
2024-01-04 12:46:06 +0800

    add noice.nvim

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -82,6 +82,22 @@ require('lazy').setup({
         version = "*",
         dependencies = 'nvim-tree/nvim-web-devicons'
     },
-    'lewis6991/gitsigns.nvim'
+    'lewis6991/gitsigns.nvim',
+    -- noice
+    {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+        }
+    }
 })
 
